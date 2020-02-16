82b2e689e379943a40b4ce1f7bd05fe32742bd13
Youssef-Fahim <61124127+Youssef-Fahim@users.noreply.github.com>
2020-02-16 21:49:43 +0100

    add config v1

---
+++ config.py
@@ -0,0 +1,12 @@
+RESNET50_DEFAULT_IMG_WIDTH = 224
+MARGIN = 0
+TRAIN_BATCH_SIZE = 16
+VALIDATION_SIZE = 100
+
+ORIGINAL_IMGS_DIR = 'male_pictures'
+ORIGINAL_IMGS_INFO_FILE = 'male_weights.csv'
+AGE_TRAINED_WEIGHTS_FILE = 'age_only_resnet50_weights.061-3.300-4.410.h5'
+CROPPED_IMGS_DIR = 'female_prisoners'
+CROPPED_IMGS_INFO_FILE = 'female_prisoners_kg_data.csv'
+TOP_LAYER_LOG_DIR = 'logs/top_layer'
+ALL_LAYERS_LOG_DIR = 'logs/all_layers'
